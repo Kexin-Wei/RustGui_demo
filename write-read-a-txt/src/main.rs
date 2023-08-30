9d6bdf9903b3cc2f967e2deb287ab8bbca7f703b
KK <wkx611@outlook.com>
2023-08-30 21:47:10 +0800

    [update] write and read txt

--- write-read-a-txt/src/main.rs
+++ write-read-a-txt/src/main.rs
@@ -5,7 +5,14 @@ fn main() {
     let file_name = "a_txt.txt";
     let contents = "This a string, which is written in a txt file.";
     match write_a_string_to_a_txt(file_name, contents) {
-        Ok(()) => println!("File is written successfully"),
+        Ok(()) => println!("File is written successfully."),
+        Err(error) => eprintln!("Error met: {}", error),
+    }
+
+    println!("Let's read what we wrote just now.");
+
+    match read_from_txt(file_name) {
+        Ok(buffer) => println!("Check what's in the file: {}", buffer),
         Err(error) => eprintln!("Error met: {}", error),
     }
 }
@@ -14,4 +21,12 @@ fn write_a_string_to_a_txt(file_name: &str, contents: &str) -> std::io::Result<(
     let mut file = File::create(file_name)?;
     file.write_all(contents.as_bytes())?;
     Ok(())
+}
+
+fn read_from_txt(file_name: &str) -> Result<String, std::io::Error> {
+    let mut file = File::open(file_name)?;
+    let mut buffer = String::new();
+
+    file.read_to_string(&mut buffer)?;
+    Ok(buffer)
 }
